a95a09d6c21eb5459d04ea758b169ccd91f69abe
Ram Janam Yadav <ramjanamjnvj1972@gmail.com>
2023-01-04 21:13:33 +0530

    completed env implementation

--- rl/env.py
+++ rl/env.py
@@ -1,27 +1,54 @@
 #creating reinforcement learning environment
 import numpy as np
 import gym
+import random
 from gym.spaces import Box, Discrete, Sequence, MultiBinary, Tuple, MultiDiscrete
 from stable_baselines3 import A2C,DQN,PPO
 
 class RlEnv(gym.Env):
     def __init__(self):
         super(RlEnv, self).__init__()
-        self.action_space = MultiDiscrete([2, 4, 4])
-        self.observation_space = Box(low=-1,high=1,shape=(4,4),dtype=np.int8)
+        self.action_space = Discrete(3)
+        self.observation_space = Box(low=np.array([0]),high=np.array([100]))
+        self.state = 38+random.randint(-3,3)
+        self.shower_length = 60
 
     def reset(self):
-        pass
+        self.state = 38+random.randint(-3,3)
+        self.shower_length = 60
+        return self.state 
 
     def step(self, action):
-        pass
+        self.state+=action-1
+        self.shower_length-=1
+
+        if self.state >=37 and self.state <=39:
+            reward = 1
+        else:
+            reward = -1
+
+        # Check if shower is done
+        if self.shower_length <= 0:
+            done = True
+        else:
+            done = False
+
+        # Apply temperature noise
+        #self.state += random.randint(-1,1)
+        # Set placeholder for info
+        info = {}
+
+        # Return step information
+        return self.state, reward, done, info
+
+    def render(self):
+        pass 
 
 
 env = RlEnv()
 env.reset()
-print(env.action_space.sample())
-print(env.observation_space.sample())
+print(env.action_space.n)
+print(env.observation_space.shape)
 
-#model = PPO('MlpPolicy', env, verbose=1)
-#model.learn(total_timesteps = 10)
+#model = A2C("MlpPolicy", env).learn(total_timesteps=1000)
 
